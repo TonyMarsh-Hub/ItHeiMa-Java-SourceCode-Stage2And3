0117f2c08a36e7e4ae77c081b95ddc05291e2a58
tonymarsh <tonymarsh0331@gmail.com>
2022-10-06 18:43:14 +0800

    :beers: 导入第二版的代码

---
+++ OOP-InfoManager/src/version2/factory/StudentDaoFactory.java
@@ -0,0 +1,11 @@
+package version2.factory;
+
+import com.itheima.edu.info.manager.dao.BaseStudentDao;
+import com.itheima.edu.info.manager.dao.OtherStudentDao;
+import com.itheima.edu.info.manager.dao.StudentDao;
+
+public class StudentDaoFactory {
+    public static BaseStudentDao getStudentDao(){
+        return new OtherStudentDao();
+    }
+}
