65306e192c9899f37cd29a16db93d971cf0c992d
tonymarsh <tonymarsh0331@gmail.com>
2022-10-19 16:27:06 +0800

    :tada: import charStream code.

---
+++ StreamAndIO/src/IO/CharStream/CharStreamDemo1.java
@@ -0,0 +1,15 @@
+package IO.CharStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CharStreamDemo1 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("charstream\\a.txt");
+        int b;
+        while ((b = fis.read()) != -1) {
+            System.out.println((char) b);
+        }
+        fis.close();
+    }
+}
