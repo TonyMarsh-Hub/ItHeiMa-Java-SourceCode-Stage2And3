213e05ec3304be4fcefada76077809159237e48a
tonymarsh <tonymarsh0331@gmail.com>
2022-11-03 00:31:19 +0800

    :art: format the code.

--- NetWorkPrograming/src/socketDemo/socketdemo2/ServerDemo.java
+++ NetWorkPrograming/src/socketDemo/socketdemo2/ServerDemo.java
@@ -12,17 +12,21 @@ public class ServerDemo {
     public static void main(String[] args) throws IOException {
         //1.找码头     ---- 表示接收端从10000端口接收数据的.
         DatagramSocket ds = new DatagramSocket(10000);
+
         //2,创建一个新的箱子
         byte[] bytes = new byte[1024];
         DatagramPacket dp = new DatagramPacket(bytes, bytes.length);
+
         //3.接收礼物,把礼物放到新的箱子中
         System.out.println("-----------接收前----------");
         ds.receive(dp);
         System.out.println("------------接收后---------");
+
         //4.从新的箱子里面获取礼物
         // byte[] data = dp.getData();
         int length = dp.getLength();
         System.out.println(new String(bytes, 0, length));
+
         //5.拿完走羊
         ds.close();
     }
--- NetWorkPrograming/src/socketDemo/socketdemo4/ClientDemo.java
+++ NetWorkPrograming/src/socketDemo/socketdemo4/ClientDemo.java
@@ -5,7 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class ClinetDemo {
+public class ClientDemo {
     public static void main(String[] args) throws IOException {
         DatagramSocket ds = new DatagramSocket();
 
