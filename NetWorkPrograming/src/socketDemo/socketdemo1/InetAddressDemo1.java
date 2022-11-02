6eb0e2d139211fb3ac8d3aaa0ca2d33d643566bf
tonymarsh <tonymarsh0331@gmail.com>
2022-11-03 00:12:26 +0800

    :bug: fix typo.

--- NetWorkPrograming/src/socketDemo/socketdemo1/InetAddressDemo1.java
+++ NetWorkPrograming/src/socketDemo/socketdemo1/InetAddressDemo1.java
@@ -8,7 +8,7 @@ import java.net.UnknownHostException;
 //                          确定主机名称的IP地址。主机名称可以是机器名称，也可以是IP地址
 //String getHostName ()     获取此IP地址的主机名
 //String getHostAddress ()  返回文本显示中的IP地址字符串
-public class InetadressDemo1 {
+public class InetAddressDemo1 {
     public static void main(String[] args) throws UnknownHostException {
         InetAddress address = InetAddress.getByName("沉迷代码");
 
