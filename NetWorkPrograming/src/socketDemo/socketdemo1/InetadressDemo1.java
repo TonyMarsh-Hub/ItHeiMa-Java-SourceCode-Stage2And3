cb38c9d565fdfac7c663f7816a1ab7677b5fc75b
tonymarsh <tonymarsh0331@gmail.com>
2022-11-03 00:11:00 +0800

    :art: change codeZWSP into space.

--- NetWorkPrograming/src/socketDemo/socketdemo1/InetadressDemo1.java
+++ NetWorkPrograming/src/socketDemo/socketdemo1/InetadressDemo1.java
@@ -4,10 +4,10 @@ package socketDemo.socketdemo1;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-//static InetAddress getByName​(String host)
+//static InetAddress getByName (String host)
 //                          确定主机名称的IP地址。主机名称可以是机器名称，也可以是IP地址
-//String getHostName​()     获取此IP地址的主机名
-//String getHostAddress​()  返回文本显示中的IP地址字符串
+//String getHostName ()     获取此IP地址的主机名
+//String getHostAddress ()  返回文本显示中的IP地址字符串
 public class InetadressDemo1 {
     public static void main(String[] args) throws UnknownHostException {
         InetAddress address = InetAddress.getByName("沉迷代码");
