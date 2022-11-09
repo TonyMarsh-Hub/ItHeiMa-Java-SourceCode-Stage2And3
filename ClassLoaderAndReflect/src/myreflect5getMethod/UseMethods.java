358081303d66f44474bab571b3caa02e6cac75c5
tonymarsh <tonymarsh0331@gmail.com>
2022-11-09 18:43:25 +0800

    :art: format the code.

--- ClassLoaderAndReflect/src/myreflect5getMethod/UseMethods.java
+++ ClassLoaderAndReflect/src/myreflect5getMethod/UseMethods.java
@@ -8,13 +8,13 @@ import java.lang.reflect.Method;
  */
 public class UseMethods {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
-//        Object invoke​(Object obj, Object... args)：运行方法
-//        参数一：用obj对象调用该方法
-//        参数二：调用方法的传递的参数（如果没有就不写）
-//        返回值：方法的返回值（如果没有就不写）
+        //Object invoke(Object obj, Object... args)：运行方法
+        //参数一：用obj对象调用该方法
+        //参数二：调用方法的传递的参数（如果没有就不写）
+        //返回值：方法的返回值（如果没有就不写）
 
         //1.获取class对象
-        Class clazz = Class.forName("com.itheima.myreflect5.Student");
+        Class<?> clazz = Class.forName("myreflect5.Student");
         //2.获取里面的Method对象  function4
         Method method = clazz.getMethod("function4", String.class);
         //3.运行function4方法就可以了
