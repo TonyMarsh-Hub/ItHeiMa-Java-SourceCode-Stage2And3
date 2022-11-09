3eb80aa8669c95403594f250afd2416bc8160001
tonymarsh <tonymarsh0331@gmail.com>
2022-11-09 16:56:45 +0800

    :art: format the code.

--- ClassLoaderAndReflect/src/myreflect2/ReflectDemo1.java
+++ ClassLoaderAndReflect/src/myreflect2/ReflectDemo1.java
@@ -5,22 +5,25 @@ package myreflect2;
  */
 public class ReflectDemo1 {
     public static void main(String[] args) throws ClassNotFoundException {
-        //1.Class类中的静态方法forName("全类名")
-        //全类名:包名 + 类名
-        Class clazz = Class.forName("com.itheima.myreflect2.Student");
-        System.out.println(clazz);
+        // 1.Class类中的静态方法forName("全类名")
+        // 全类名:包名 + 类名
+        Class<?> myClass1 = Class.forName("myreflect2.Student");
 
-        //2.通过class属性来获取
-        Class clazz2 = Student.class;
-        System.out.println(clazz2);
+        // 2.通过class属性来获取
+        Class<Student> myClass2 = Student.class;
 
-        //3.利用对象的getClass方法来获取class对象
-        //getClass方法是定义在Object类中.
+        // 3.利用对象的getClass方法来获取class对象
+        // getClass方法是定义在Object类中.
         Student s = new Student();
-        Class clazz3 = s.getClass();
-        System.out.println(clazz3);
+        Class<? extends Student> myClass3 = s.getClass();
 
-        System.out.println(clazz == clazz2);
-        System.out.println(clazz2 == clazz3);
+        // 验证三个对象是否是同一个对象
+        // 由于方法2和3已经显式声明获取的是Student的class类对象，所以myClass2 == myClass3恒为true，
+        // 我们只要比较这两个对象和myClass1是否相等即可
+        System.out.println(myClass1 == myClass2 & myClass1 == myClass3);
+        // 或者直接打印
+        System.out.println(myClass1);
+        System.out.println(myClass2);
+        System.out.println(myClass3);
     }
 }
