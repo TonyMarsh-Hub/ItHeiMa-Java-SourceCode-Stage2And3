a6f96e9417cc25bc53cda34645bfb1fcf3833dca
tonymarsh <tonymarsh0331@gmail.com>
2022-11-23 19:59:49 +0800

    :beers: dao层 根据id批量删除

--- brand-case/src/main/java/com/itheima/mapper/BrandMapper.java
+++ brand-case/src/main/java/com/itheima/mapper/BrandMapper.java
@@ -2,6 +2,7 @@ package com.itheima.mapper;
 
 import com.itheima.pojo.Brand;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,6 +12,7 @@ public interface BrandMapper {
 
     /**
      * 查询所有
+     *
      * @return
      */
     @Select("select * from tb_brand")
@@ -19,8 +21,16 @@ public interface BrandMapper {
 
     /**
      * 添加数据
+     *
      * @param brand
      */
     @Insert("insert into tb_brand values(null,#{brandName},#{companyName},#{ordered},#{description},#{status})")
     void add(Brand brand);
+
+    /**
+     * 根据id进行批量删除
+     *
+     * @param ids 传递的多个id
+     */
+    void deleteByIds(@Param("ids") int[] ids);
 }
