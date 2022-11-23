5e40ea2b061fdf7ddfdc97dfaf2bf86319886247
tonymarsh <tonymarsh0331@gmail.com>
2022-11-23 19:47:15 +0800

    :beers: 将其他Servlet的处理逻辑整合到新的BrandServlet中

--- brand-case/src/main/java/com/itheima/web/servlet/BrandServlet.java
+++ brand-case/src/main/java/com/itheima/web/servlet/BrandServlet.java
@@ -1,16 +1,46 @@
 package com.itheima.web.servlet;
 
+import com.alibaba.fastjson.JSON;
+import com.itheima.pojo.Brand;
+import com.itheima.service.BrandService;
+import com.itheima.service.impl.BrandServiceImpl;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/brand/*")
 public class BrandServlet extends BaseServlet {
-    public void selectAll(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("查询所有");
+
+    private final BrandService brandService = new BrandServiceImpl();
+
+    public void selectAll(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //1. 调用service查询
+        List<Brand> brands = brandService.selectAll();
+
+        //2. 转为JSON
+        String jsonString = JSON.toJSONString(brands);
+
+        //3. 写数据
+        response.setContentType("text/json;charset=utf-8");
+        response.getWriter().write(jsonString);
     }
 
-    public void add(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("添加");
+    public void add(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //1. 接收品牌数据
+        BufferedReader br = request.getReader();
+        String params = br.readLine();//json字符串
+
+        //转为Brand对象
+        Brand brand = JSON.parseObject(params, Brand.class);
+
+        //2. 调用service添加
+        brandService.add(brand);
+
+        //3. 响应成功的标识
+        response.getWriter().write("success");
     }
 }
